190b3cc97f5a353e6d3b73d52272e10e2bd0f078
tianyingchun <tianyingchun@outlook.com>
2013-12-31 18:53:28 +0800

    add some styles here
    add some styles, and update the frame.js

--- source/app/views/Frame.js
+++ source/app/views/Frame.js
@@ -4,19 +4,42 @@ enyo.kind({
 	layoutKind: "FittableRowsLayout",
 	fit: true,
 	components:[
-		{kind: "onyx.Toolbar", content: "Hello World"},
+		{kind: "onyx.Toolbar", id:"header", components: [
+			{ classes:"logo", content:"开发者平台", ontap:"goHome" },
+			{ classes:"top-nav", tag:"ul", components: [
+				{tag:"li", classes:"nav-item", content:"首页"},
+				{tag:"li", classes:"nav-item", content:"平安官网"}
+			]}
+		]},
 		{kind: "enyo.Scroller", fit: true, components: [
 			{name: "main", classes: "nice-padding"}
 		]},
-		{kind: "onyx.Toolbar", components: [
-			{kind: "onyx.Button", content: "Tap me", ontap: "helloWorldTap"}
+		{kind: "onyx.Toolbar", id:"footer", components: [
+			{ classes:"left-aside", components: [
+				{ classes:"aside-link", tag: "ul", components: [
+					{ tag:"li", classes:"link-item", components:[
+						{tag:"a", href:"#", content:"平安"}
+					]},
+					{ tag:"li", classes:"link-item", components:[
+						{tag:"a", href:"#", content:"开发者"}
+					]},
+					{ tag:"li", classes:"link-item", components:[
+						{tag:"a", href:"#", content:"1钱包"}
+					]}
+				]},
+				{classes:"copyright", content:"© 2013 pingan 使用前必读 | 京ICP证030173号"}
+			]},
+			{ classes: "right-aside", components: [
+				{classes:"our-service", components: [
+					{classes:"dev-feedback", content:"问题反馈"},					
+					{classes:"dev-mail", content:"dev_support@pingan.com.cn"},
+					{classes:"dev-center", content:"开发者中心"}
+				]}
+			]}
 		]}
 	],
 	handlers: {
 		onGetCategoryDetail: "getCategoryDetail"
-	},
-	helloWorldTap: function(inSender, inEvent) {
-		this.$.main.setContent("The button was tapped.<br/>");
 	},
 	/**
 	 * Set view config as child client control for main frame view.
