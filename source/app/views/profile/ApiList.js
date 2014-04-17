17c815d312f5eb5d38875e9d4d327a9de651478d
tianyingchun <tianyingchun@outlook.com>
2014-04-17 17:50:09 +0800

    add new paged list draft code

--- source/app/views/profile/ApiList.js
+++ source/app/views/profile/ApiList.js
@@ -1,44 +1,34 @@
 enyo.kind({
 	name: "Master.views.profile.ApiList",
 	kind: "Master.View",
-	classes: "api-list",
+	classes: "list-wrapper",
 	events:{
 		onDeleteApiItem:""
+	},
+	handlers:{
+		onActionButtonTap:"rowActionButtonTap"
 	},
 	components: [
 		{name:"message",kind:"widgets.base.Spinner", message: Master.locale.get("LOAD_API_LIST", "message")},
 		{kind:"onyx.Groupbox", name:"listWrapper", showing:false, components: [
-			{kind: "onyx.GroupboxHeader", components:[
+			{classes:"list-header", components:[
 				{content:"API列表", classes:"list-title"},
-				{content:"添加",kind:"onyx.Button", classes:"add-new-button", ontap:"addNewApi"}
-			]},
-			{name:"apiList", kind: "List", mutiSelect: false, onSetupItem: "apiListSetupItem", components: [
-				{name: "item", classes:"list-item", tag: "ul", ontap:"itemTap", components: [
-					{name:"apiId", tag:"li", classes: "id"},
-					{name:"apiName", tag:"li", classes: "name"},
-					{name:"apiKey", tag:"li", classes: "key"},
-					{tag:"li", action:"addnew", classes: "add-new", content:Master.locale.get("ACTION_ADD", "label")},
-					{tag:"li", action:"edit", classes: "edit", content:Master.locale.get("ACTION_EDIT", "label")},
-					{tag:"li", action:"remove", classes: "remove", content:Master.locale.get("ACTION_REMOVE", "label")}
+				{tag:"a", classes:"btn",ontap:"addNewApi", components:[
+					{tag:"i", classes:"icon-plus"},
+					{tag:"span",content:"添加"}
 				]}
-			]}
+			]},
+			{name:"docList", kind:"widgets.lists.PagedList",fields: ['apiId','apiName','apiKey']}
 		]}
 	],
 	showApiListUI: function (viewModel, data){
 		this.zLog("viewModel: ", viewModel,"data: ", data);
 		this.cachedAPIList = viewModel.records;
-		this.$.apiList.setCount(this.cachedAPIList.length);
-		this.$.apiList.reset();
+		this.$.docList.set("source",this.cachedAPIList);
+		// this.$.apiList.setCount(this.cachedAPIList.length);
+		// this.$.apiList.reset();
 		this.$.message.hide();
 		this.$.listWrapper.show();
-	},
-	apiListSetupItem: function (inSender,inEvent) {
-		var index = inEvent.index;
-		var currItem = this.cachedAPIList[index];
-		this.$.apiId.setContent(currItem.apiId);
-		this.$.apiName.setContent(currItem.apiName);
-		this.$.apiKey.setContent(currItem.apiKey);
-		this.$.item.setShowing(currItem.isDisplay);
 	},
 	addNewApi: function (inSender,inEvent) {
 		this.location("profile/api/add");
@@ -54,22 +44,34 @@ enyo.kind({
 	deleteApiItem: function (apiId, apiKey) {
 		this.doDeleteApiItem({apiId: apiId, apiKey: apiKey});
 	},
-	itemTap: function (inSender, inEvent) {
-		// this.zLog("originator: ", inEvent);
-		var originator = inEvent.originator;
-		var index = inEvent.index;
-		var currItem = this.cachedAPIList[index];
+	//*@private helper.
+	findRowItemData: function(apiKey) {
+		var find = null;
+		for (var i = 0; i < this.cachedAPIList.length; i++) {
+			var item = this.cachedAPIList[i];
+			if(item.apiKey==apiKey) {
+				find = item;
+				break;
+			}
+		};
+		return find;
+	},
+	//*@action button tap handler.
+	rowActionButtonTap: function (inSender, inEvent) {
+		var action = inEvent.action;
+		var key = inEvent.key;
 		var location = "";
-		switch(originator.action) {
-			case "addnew":
-			location = "profile/api/add";
-			break;
+		switch(action) {
+			case "add":
+				location = "profile/api/add";
+				break;
 			case "edit":
-			location = "profile/api/edit/"+ currItem.apiKey
-			break;
+				location = "profile/api/edit/"+ key;
+				break;
 			case "remove":
+				var currItem = this.findRowItemData(key);
 				this.confirmDeleteApiItem(currItem.apiId, currItem.apiKey, currItem.apiName);
-			break;
+				break; 
 		}
 		if (location) {
 			this.location(location);
