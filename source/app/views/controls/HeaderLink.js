b12d1b92f30a351b30952fdef9b28b85cf66b437
tianyingchun <tianyingchun@outlook.com>
2014-05-04 17:10:08 +0800

    add new normallogin dialog widget, and simple tabs widget

--- source/app/views/controls/HeaderLink.js
+++ source/app/views/controls/HeaderLink.js
@@ -1,9 +1,20 @@
 enyo.kind({
 	name:"Master.views.controls.HeaderLink",
 	classes:"navbar",
+	mixins: [
+		"Master.ClassSupport"
+	],
+	handlers: {
+		ontap:"loginTap"
+	},
 	components: [
 		{ tag:"ul", classes:"navbar-inner", components: [
 				{ tag:"li", classes:"item text", content:"欢迎使用壹钱包！"},
+				{ tag:"li", classes:"item",components: [
+					{ tag: "span", action:"login", content:"登陆"},
+					{ tag: "span", content:"-"},
+					{ tag: "span", action:"register", content:"注册"}
+				]},
 				{ tag:"li", classes:"item",components: [
 					{ tag: "a", attributes:{ target:"_blank", href:"http://dev.1qianbao.com/blog/" }, content:"官方博客"}
 				]},
@@ -16,5 +27,17 @@ enyo.kind({
 					{ tag: "a", attributes:{ target:"_blank", href:"http://www.1qianbao.com/" }, content:"壹钱包官网"}
 				]}
 		]}
-	]
+	],
+	loginTap: function (inSender, inEvent) {
+		this.zLog(inEvent);
+		var $originator = inEvent.originator;
+		if ($originator.action == "login") { 
+			var config = {};
+			var loginDialog = new widgets.dialog.NormalLoginDialog(config);
+			loginDialog.show();
+		} else{
+			// loginDialog.setActiveIndex(1)
+		}
+		return true;
+	}
 });
