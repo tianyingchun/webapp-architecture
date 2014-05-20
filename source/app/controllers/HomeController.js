7a67e60397a40eb173d6c60406653219432c3ed3
tianyingchun <tianyingchun@outlook.com>
2014-05-20 19:00:19 +0800

    bug fix

--- source/app/controllers/HomeController.js
+++ source/app/controllers/HomeController.js
@@ -27,7 +27,7 @@ enyo.kind({
 	},
 	fetchDefaultApiDetailInfo: function (viewModel) {
  		var apiDetail = viewModel.records[0] || null;
- 		var apiModel = this.getModelInstance("Master.models.apipool.ApiItem", {id:""});
+ 		var apiModel = this.getModelInstance("Master.models.apipool.ApiItem", {id:apiDetail.id});
 		apiModel.setObject(apiDetail);
 		var viewData = {
 			action: "showApiDetailUI"
