ebad7d8b0ab27f4f2632ae60c98d317b4690bc1d
tianyingchun <tianyingchun@outlook.com>
2014-09-02 10:49:47 +0800

    update add create new user feature

--- source/app/models/user/UserModel.js
+++ source/app/models/user/UserModel.js
@@ -1,55 +1,72 @@
 enyo.kind({
-	name: "Master.models.user.UserModel",
-	kind: "Master.Model",
-	apis: {
-		login:{
-			url: "/login",
-			cache:false,
-			dto: "userInfoDTO"
-		},
-		resetPwd: {
-			url: "/user/changePwd",
-			cache: false,
-			dto: "userInfoDTO"
-		}
-	},
-	attributes: {
-		username: "",
-		password: "",
-		lastActive:"",
-		token: "",
-		role: "admin"// default roles is admin.
-	},
-	// user login.
-	login: function (user, fn) {
-		this.username = user.username;
-		this.password = user.password;
-		this.commit({
-			apiKey: "login", 
-			method: "POST",
-			data: {
-				username: user.username,
-				password: user.password
-			},
-			callback: fn
-		});
-	},
-	resetPassword: function (user, fn) {
-		this.commit({
-			apiKey: "resetPwd",
-			method:"POST",
-			data: {
-				username: user.username,
-				password: user.password
-			},
-			callback: fn
-		});
-	},
-	// login callback data dto.
-	userInfoDTO: function(data, options) {
-		return {
-			token: data.token,
-			username: data.username
-		};
-	}
+    name: "Master.models.user.UserModel",
+    kind: "Master.Model",
+    apis: {
+        login: {
+            url: "/login",
+            cache: false,
+            dto: "userInfoDTO"
+        },
+        resetPwd: {
+            url: "/user/changePwd",
+            cache: false
+        },
+        createNewUser: {
+            url: "/user/createNewUser",
+            cache: false,
+            dto: "userInfoDTO"
+        }
+    },
+    attributes: {
+        username: "",
+        password: "",
+        lastActive: "",
+        token: "",
+        roles: "admin" // default roles is admin.
+    },
+    // user login.
+    login: function(user, fn) {
+        this.username = user.username;
+        this.password = user.password;
+        this.commit({
+            apiKey: "login",
+            method: "POST",
+            data: {
+                username: user.username,
+                password: user.password
+            },
+            callback: fn
+        });
+    },
+    createNewUser: function(user, fn) {
+        this.commit({
+            apiKey: "createNewUser",
+            method: "POST",
+            data: {
+                username: user.username,
+                password: user.password,
+                roles: user.roles
+            },
+            callback: fn
+        });
+    },
+    resetPassword: function(user, fn) {
+        this.commit({
+            apiKey: "resetPwd",
+            method: "POST",
+            data: {
+                username: user.username,
+                password: user.password
+            },
+            callback: fn
+        });
+    },
+    // login callback data dto.
+    userInfoDTO: function(data, options) {
+        return {
+            token: "",
+            username: data.UserName,
+            roles: data.Roles
+        };
+    }
 });
--- source/app/views/user/Detail.js
+++ source/app/views/user/Detail.js
@@ -20,15 +20,40 @@ enyo.kind({
 				{classes:"result", name:"modifyResult"}
 			]}
 		]},
+		{formType:"createUser", submitButtonStyles:"btn btn-primary",submitButtonText:"添加新用户", kind:"widgets.forms.FormDecorator", components: [
+			{kind:"onyx.Groupbox", components: [
+				{kind: "onyx.GroupboxHeader", content: "添加新的用户信息"},
+				{classes:"form-item", components:[
+					{ classes:"title", content:"用户名: "},
+					{ name:"new_user_username", placeholder:"admin name", kind:"widgets.forms.InputDecorator", tipMessage:"用户名称必须填写！", validation: {required:"必填字段！"}}
+				]},
+				{classes:"form-item", components:[
+					{ classes:"title", content:"输入密码: "},
+					{ name:"new_user_password", placeholder:"password",type:"password", kind:"widgets.forms.InputDecorator", tipMessage:"用户密码必须填写！", validation: {required:"必填字段！"}}
+				]},
+				{classes:"form-item", components:[
+					{ classes:"title", content:"输入角色: "},
+					{ name:"new_user_roles", placeholder:"staff", kind:"widgets.forms.InputDecorator", tipMessage:"用户Role必须填写,多个ROLE 用逗号隔开！", validation: {required:"必填字段！"}}
+				]},
+				{classes:"result", name:"createNewUserResult"}
+			]}
+		]}
 	],
 	formValidationSubmit: function (inSender, inEvent) {
 		var validationResult = inEvent;
 		var $originator = inEvent.originator;
 		var formType = $originator.formType;
 		this.zLog("form validation result", validationResult);
-		// success/failed.
-		if (validationResult.status =="success") {
-			this.resetPassword();
+		if(formType=="login"){ 
+			// success/failed.
+			if (validationResult.status =="success") {
+				this.resetPassword();
+			}
+		} else {
+			// success/failed.
+			if (validationResult.status =="success") {
+				this.createNewUser();
+			}
 		}
 
 		return true;
@@ -53,6 +78,28 @@ enyo.kind({
 			this.$.modifyResult.addClass("error");
 			this.$.modifyResult.removeClass("success");
 			this.$.modifyResult.setContent(restInfo.retMessage);
+		}
+	},
+	createNewUser: function(){
+		var userModel = new Master.models.user.UserModel();
+		var user = {
+			username: this.$.new_user_username.getValue(),
+			password: this.$.new_user_password.getValue(),
+			roles: this.$.new_user_roles.getValue()
+		};
+		userModel.createNewUser(user, this.bindSafely("createNewUserCB"));
+	},
+	createNewUserCB: function (viewModel) {
+		this.zLog(viewModel);
+		var restInfo = viewModel.restInfo;
+		if(restInfo.retCode==="1") {
+			this.$.createNewUserResult.removeClass("error");
+			this.$.createNewUserResult.addClass("success");
+			this.$.createNewUserResult.setContent("创建新用户成功！"); 
+		} else {
+			this.$.createNewUserResult.addClass("error");
+			this.$.createNewUserResult.removeClass("success");
+			this.$.createNewUserResult.setContent(restInfo.retMessage);
 		}
 	}
 });
